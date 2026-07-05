93d1c0dfb30dbe783613b78d5817fdaa3d159399
FGARMY <flaminggarena@gmail.com>
2026-07-06 03:16:32 +0530

    Disable product card animations on mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -166,6 +166,7 @@ const Home = () => {
             {products.map((p, i) => (
               <motion.div
                 key={i}
+                className="product-card-wrapper"
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true, margin: "-50px" }}
