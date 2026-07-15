217f9d1bede1a5c1ecae3d316c701c92d6533753
FGARMY <flaminggarena@gmail.com>
2026-07-15 14:55:34 +0530

    Number Change +91 9711492120

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,7 +6,7 @@ export const SITE_CONFIG = {
       "Trilokpuri,",
       "New Delhi."
     ],
-    phone: "+91 7982194994, +91 8882567498",
+    phone: "+91 7982194994, +91 9711492120",
     email: "info@adbpack.com",
     hours: "Mon - Sat: 9:00 AM - 6:00 PM"
   },
